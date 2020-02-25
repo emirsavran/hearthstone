ebdefbff3f13b62f2770eebc08919172a93d514e
Emir Savranogullari <emir@emirsavran.com>
2020-02-25 13:59:05 +0300

    Create API class and findCards method

---
+++ src/api/index.js
@@ -0,0 +1,22 @@
+const rapidapiHost = 'omgvamp-hearthstone-v1.p.rapidapi.com';
+const rapidapiKey = 'ad19c34cc9msh781c297aac24bb4p155dd2jsnf75d4c846f35';
+
+class API {
+  constructor(host, key) {
+    this.fetch = path => {
+      return fetch(`https://${host}/${path}`, {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-host': host,
+          'x-rapidapi-key': key,
+        },
+      });
+    };
+  }
+
+  findCards() {
+    return this.fetch('cards');
+  }
+}
+
+export default new API(rapidapiHost, rapidapiKey);
