18c2e86925f5792ebc55679423c29b122264f0e5
Emir Savranogullari <emir@emirsavran.com>
2020-02-26 13:41:35 +0300

    Create and style search input

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -1,8 +1,33 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {Text, TextInput, StyleSheet, Dimensions} from 'react-native';
 
-const Search = () => {
+const Search = ({navigation}) => {
+  const [value, onChangeText] = useState('');
+
+  navigation.setOptions({
+    headerTitle: () => (
+      <TextInput
+        style={styles.input}
+        value={value}
+        onChangeText={onChangeText}
+        placeholder="Enter card name..."
+        autoCapitalize="none"
+        autoCorrect={false}
+        autoFocus
+      />
+    ),
+  });
+
   return <Text>Search Screen</Text>;
 };
+
+const styles = StyleSheet.create({
+  input: {
+    width: Dimensions.get('window').width - 100,
+    height: 40,
+    borderBottomWidth: 1,
+    borderBottomColor: '#dddddd',
+  },
+});
 
 export default Search;
